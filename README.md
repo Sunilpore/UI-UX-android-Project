fd6462ff65703e68fb473bde84f10a0427cd778a
Sunil <sunilyahoo95@gmail.com>
2021-07-21 19:27:05 +0530

    add coloumn layout and styles

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UI-UX-android-Project
 Example App in Compose MVVM architecture
+
+Topics covered->
+- Add Coloumns Layout for vertical scroll
+- Add images to imageview, textStyle to textview
+
+
+Image Resource changes to set Image using compose:-
+https://stackoverflow.com/a/66527392
+
+Used Reference code:-~
+https://github.com/mitchtabian/MVVMRecipeApp/tree/column-layouts
--- app/src/main/java/com/foodyapp/MainActivity.kt
+++ app/src/main/java/com/foodyapp/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.foodyapp
 
 
+
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 
+@ExperimentalUnitApi
 class MainActivity : AppCompatActivity() {
 
 
@@ -20,16 +30,48 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
 
         setContent {
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ){
-                Text(text = "Welcome to Compose")
-                Spacer(modifier = Modifier.padding(top=10.dp))
-                Button(onClick = {  })
-                {
-                    Text(text = "CLICK HERE")
+
+            LazyColumn(
+
+                modifier = Modifier
+                    .background(
+                    color = Color(0x4DCDCED1)
+                )
+            ) {
+
+                item {
+
+                    Image(bitmap = ImageBitmap.imageResource(id = R.drawable.happy_meal) ,
+                        contentDescription = "",
+                        modifier = Modifier.height(300.dp),
+                        contentScale = ContentScale.Crop)
+
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(text = "Happy Meal",
+                            style = TextStyle(
+                                fontSize = TextUnit(24.0f, TextUnitType.Sp)
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.padding(top = 10.dp))
+                        Text(text = "800 Calories",
+                            style = TextStyle(
+                                color = Color(0xFF2B315A),
+                                fontSize = TextUnit(16.0f,TextUnitType.Sp))
+                        )
+
+                        Spacer(modifier = Modifier.padding(top = 10.dp))
+                        Text(text = "Rs. 40",
+                            style = TextStyle(
+                                color = Color(0xFF0EA214),
+                                fontSize = TextUnit(14.0f,TextUnitType.Sp)))
+                    }
+
                 }
             }
+
         }
 
     }
